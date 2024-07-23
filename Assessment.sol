3e9b5e72c9d1030abf3e56d5c33a87f419fbc07f
Sandeep1925 <sandeepkaur1925@gmail.com>
2024-07-23 23:28:17 +0530

    Assessment.sol

--- Assessment.sol
+++ Assessment.sol
@@ -1,58 +1,41 @@
-
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.20;
 
-contract MyToken {
-    uint256 public totalSupply;
-    string public name;
-    string public symbol;
-    address public owner;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-    mapping(address => uint256) public balances;
+contract MyToken is ERC20, Ownable {
 
-    event Transfer(address indexed from, address indexed to, uint256 amount);
     event Mint(address indexed to, uint256 amount);
     event Burn(address indexed from, uint256 amount);
 
-    constructor(string memory _name, string memory _symbol, address _owner) {
-        name = _name;
-        symbol = _symbol;
-        owner = _owner;
-        totalSupply = 1000000;
-        balances[msg.sender] = totalSupply;
-    }
-
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Only the owner can call this function");
-        _;
+    constructor(string memory _name, string memory _symbol, address initialOwner) 
+        ERC20(_name, _symbol) 
+        Ownable(initialOwner) // Pass the initialOwner to the Ownable constructor
+    {
+        _mint(initialOwner, 100 * 10 ** decimals()); // Mint initial supply to the initialOwner
     }
 
     function mint(address to, uint256 amount) public onlyOwner {
         require(amount > 0, "Amount must be greater than zero");
-        balances[to] += amount;
-        totalSupply += amount;
+        _mint(to, amount);
         emit Mint(to, amount);
-        emit Transfer(address(0), to, amount);
     }
 
     function burn(uint256 amount) public {
         require(amount > 0, "Amount must be greater than zero");
-        require(balances[msg.sender] >= amount, "Not enough tokens to burn");
-        balances[msg.sender] -= amount;
-        totalSupply -= amount;
+        _burn(msg.sender, amount);
         emit Burn(msg.sender, amount);
-        emit Transfer(msg.sender, address(0), amount);
     }
 
-    function transfer(address to, uint256 amount) public {
+    function transfer(address to, uint256 amount) public override returns (bool) {
         require(amount > 0, "Amount must be greater than zero");
-        require(balances[msg.sender] >= amount, "Not enough tokens to transfer");
-        balances[msg.sender] -= amount;
-        balances[to] += amount;
-        emit Transfer(msg.sender, to, amount);
+        require(balanceOf(msg.sender) >= amount, "Not enough tokens to transfer");
+        _transfer(msg.sender, to, amount);
+        return true; // Return true as per ERC20 standard
     }
 
     function getBalance(address account) public view returns (uint256) {
-        return balances[account];
+        return balanceOf(account);
     }
 }
